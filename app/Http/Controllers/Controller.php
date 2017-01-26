4092a0205adfceb4e7fb14dd806f3e35f12037bb
Stephane Mullings <stephane@wondersharemediaservermacprovideo.home>
2017-01-26 11:35:32 +0100

    mise en place d'un acces au user et user role pour tout les controlleurs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,29 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Cache;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    protected  $user_role;
+    protected  $user;
+    public function __construct(){
+        
+        $this->middleware(function ($request, $next) {
+            
+            $this->user_role = Cache::remember('user_role', 60, function () {
+                return Auth::user()->roles->first()->name;
+            });
+            
+            $this->user = Cache::remember('user', 60, function () {
+                return Auth::user();
+            });
+    
+             
+            return $next($request);
+            
+        });
+    }
 }
