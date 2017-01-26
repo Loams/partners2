66c2a8f154576de019540bc793c2ba03721f9558
Stephane Mullings <stephane@wondersharemediaservermacprovideo.home>
2017-01-26 12:44:49 +0100

    modification de User pour etre authentifiable

--- app/User.php
+++ app/User.php
@@ -2,12 +2,17 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model {
+class User extends Authenticatable {
 
+    use Notifiable;
+    
 	protected $table = 'users';
+    
 	public $timestamps = true;
+    
 	protected $fillable = array('gender', 'firstname', 'lastname', 'function', 'email', 'phone', 'partners_id', 'password');
 	protected $hidden = array('password', 'remember_token');
 
